4720b182ec6fa736b1ea9a44e665734268ec5145
Enrico Guiraud <enrico.guiraud@pm.me>
2023-10-01 17:56:37 -0600

    Prefer EXPECT_FLOAT_EQ over DOUBLE for floats

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,26 +11,26 @@ TEST(Graph, SumEval) {
   const Inputs inputs{{"x", 3.}};
 
   const Graph g1 = x + x;  // Var + Var
-  EXPECT_DOUBLE_EQ(g1.eval(inputs), 6.);
+  EXPECT_FLOAT_EQ(g1.eval(inputs), 6.);
 
   const Graph g2 = c + c;  // Const + Const
-  EXPECT_DOUBLE_EQ(g2.eval(inputs), 4.);
+  EXPECT_FLOAT_EQ(g2.eval(inputs), 4.);
 
   const Graph g3 = x + c;  // Var + Const
   const Graph g4 = c + x;  // Const + Var
-  for (const auto &g : {g3, g4}) EXPECT_DOUBLE_EQ(g.eval(inputs), 5.);
+  for (const auto &g : {g3, g4}) EXPECT_FLOAT_EQ(g.eval(inputs), 5.);
 
   const Graph g5 = x + g1;  // Var + Graph
   const Graph g6 = g1 + x;  // Graph + Var
-  for (const auto &g : {g5, g6}) EXPECT_DOUBLE_EQ(g.eval(inputs), 9.);
+  for (const auto &g : {g5, g6}) EXPECT_FLOAT_EQ(g.eval(inputs), 9.);
 
   const Graph g7 = c + g1;  // Const + Graph
   const Graph g8 = g1 + c;  // Graph + Const
-  for (const auto &g : {g7, g8}) EXPECT_DOUBLE_EQ(g.eval(inputs), 8.);
+  for (const auto &g : {g7, g8}) EXPECT_FLOAT_EQ(g.eval(inputs), 8.);
 
   // Graph and Graph
   const auto g9 = g1 + g1;  // Graph + Graph
-  EXPECT_DOUBLE_EQ(g9.eval(inputs), 12.);
+  EXPECT_FLOAT_EQ(g9.eval(inputs), 12.);
 }
 
 TEST(Graph, MulEval) {
@@ -39,26 +39,26 @@ TEST(Graph, MulEval) {
   const Inputs inputs{{"x", 3.}};
 
   const Graph g1 = x * x;  // Var * Var
-  EXPECT_DOUBLE_EQ(g1.eval(inputs), 9.);
+  EXPECT_FLOAT_EQ(g1.eval(inputs), 9.);
 
   const Graph g2 = c * c;  // Const * Const
-  EXPECT_DOUBLE_EQ(g2.eval(inputs), 4.);
+  EXPECT_FLOAT_EQ(g2.eval(inputs), 4.);
 
   const Graph g3 = x * c;  // Var * Const
   const Graph g4 = c * x;  // Const * Var
-  for (const auto &g : {g3, g4}) EXPECT_DOUBLE_EQ(g.eval(inputs), 6.);
+  for (const auto &g : {g3, g4}) EXPECT_FLOAT_EQ(g.eval(inputs), 6.);
 
   const Graph g5 = x * g1;  // Var * Graph
   const Graph g6 = g1 * x;  // Graph * Var
-  for (const auto &g : {g5, g6}) EXPECT_DOUBLE_EQ(g.eval(inputs), 27.);
+  for (const auto &g : {g5, g6}) EXPECT_FLOAT_EQ(g.eval(inputs), 27.);
 
   const Graph g7 = c * g1;  // Const * Graph
   const Graph g8 = g1 * c;  // Graph * Const
-  for (const auto &g : {g7, g8}) EXPECT_DOUBLE_EQ(g.eval(inputs), 18.);
+  for (const auto &g : {g7, g8}) EXPECT_FLOAT_EQ(g.eval(inputs), 18.);
 
   // Graph and Graph
   const auto g9 = g1 * g1;  // Graph * Graph
-  EXPECT_DOUBLE_EQ(g9.eval(inputs), 81.);
+  EXPECT_FLOAT_EQ(g9.eval(inputs), 81.);
 }
 
 TEST(Tests, WriteAndReadGraph) {
@@ -78,5 +78,5 @@ TEST(Tests, WriteAndReadGraph) {
 
   // check the graph still evaluates correctly
   const Graph &g2 = gs.value();
-  EXPECT_DOUBLE_EQ(g2.eval(inputs), 42.);
+  EXPECT_FLOAT_EQ(g2.eval(inputs), 42.);
 }
